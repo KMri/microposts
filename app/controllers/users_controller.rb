28c53c01a1a0d65ce4444bf0ba9f04ca4872ef38
kosukemarui <km1ur7si2k1@gmail.com>
2016-06-15 12:17:48 +0000

    create update & edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+  
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -11,9 +13,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to user_path(@user)
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    # correct_userを呼び出すため、以下は不要
+    # @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -22,5 +37,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) if @user != current_user
   end
 end
