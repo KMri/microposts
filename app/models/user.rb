6ba549904d3ab00b15599a6691598437f1bd9dec
kosukemarui <km1ur7si2k1@gmail.com>
2016-06-19 03:20:12 +0000

    add posting microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts
+  
   before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -6,6 +8,9 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  
+  # 地域についての入力判定
+  # createに対しては悪意あるユーザに更新されないようにしている
   validates :location, absence: true, 
                        on: :create
   validates :location, allow_blank: true, 
